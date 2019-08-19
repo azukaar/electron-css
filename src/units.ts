645db642b06522117441bd5217dffa86c3c40053
Yann Stepienik <azukaar@gmail.com>
2019-08-19 20:01:04 +0100

    chore: finish TS integration

--- src/units.ts
+++ src/units.ts
@@ -1,23 +1,37 @@
+export const px = (nb) => nb + 'px';
+export const cm = (nb) => nb + 'cm';
+export const mm = (nb) => nb + 'mm';
+export const inch = (nb) => nb + 'in';
+export const pt = (nb) => nb + 'pt';
+export const pc = (nb) => nb + 'pc';
+export const pct = (nb) => nb + '%';
+export const em = (nb) => nb + 'em';
+export const ex = (nb) => nb + 'ex';
+export const ch = (nb) => nb + 'ch';
+export const rem = (nb) => nb + 'rem';
+export const vw = (nb) => nb + 'vw';
+export const vh = (nb) => nb + 'vh';
+export const deg = (nb) => nb + 'deg';
+export const vmin = (nb) => nb + 'vmin';
+export const vmax = (nb) => nb + 'vmax';
+
 const units = {
-  'px': (nb) => nb + 'px',
-  'cm': (nb) => nb + 'cm',
-  'mm': (nb) => nb + 'mm',
-  'in': (nb) => nb + 'in',
-  'pt': (nb) => nb + 'pt',
-  'pc': (nb) => nb + 'pc',
-  'pct': (nb) => nb + '%',
-  'em': (nb) => nb + 'em',
-  'ex': (nb) => nb + 'ex',
-  'ch': (nb) => nb + 'ch',
-  'rem': (nb) => nb + 'rem',
-  'vw': (nb) => nb + 'vw',
-  'vh': (nb) => nb + 'vh',
-  'deg': (nb) => nb + 'deg',
-  'vmin': (nb) => nb + 'vmin',
-  'vmax': (nb) => {
-    // TODO : display warning for IE
-    return nb + 'vmax';
-  }
+  px,
+  cm,
+  mm,
+  inch,
+  pt,
+  pc,
+  pct,
+  em,
+  ex,
+  ch,
+  rem,
+  vw,
+  vh,
+  deg,
+  vmin,
+  vmax,
 };
 
 export default units;
